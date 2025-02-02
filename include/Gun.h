270d9062e2ba1baee18d28e1929136c2f517ee10
Alexey Pravilov <alexeypravilov@pub80-216.public.amu.edu.pl>
2025-02-02 16:58:06 +0100

    Added gun header

--- include/Gun.h
+++ include/Gun.h
@@ -4,11 +4,22 @@
 
 #ifndef GUN_H
 #define GUN_H
+#include "Entity.h"
+#include "Structures.h"
 
+class Gun : public Entity
+{
+protected:
+  Entity * parent{};
+  Camera2D * camera{};
+  float targetRotation{};
+public:
+  Gun() : Entity() {};
+  Gun(EntityManager * p_manager, structures::EntityData * p_data);
 
-
-class Gun {
-
+  void setParent(Entity * p_parent);
+  void setCamera(Camera2D * p_camera);
+  void update(float delta) override;
 };
 
 
