1f6e0c0f3ea5f8a2f03e022b6f72e787f1880e44
Alexey Pravilov <alexeypravilov@pub80-216.public.amu.edu.pl>
2025-02-02 15:17:27 +0100

    Added player animations

--- include/Astronaut.h
+++ include/Astronaut.h
@@ -12,6 +12,16 @@ public:
   Astronaut() : Entity() {};
   Astronaut(EntityManager * p_manager, structures::EntityData * p_data) : Entity(p_manager, p_data) {};
   void update(float delta) override;
+  void setCamera(Camera2D * p_camera);
+  void animate();
+private:
+  Camera2D * camera {};
+  enum ANIMATION_STATE { IDLE , RUNNING , DEAD };
+  ANIMATION_STATE animationState {};
+  structures::Vector2f velocity {};
+  float animationSpeed = 10.0f;
+  float currentTick {};
+  float targetRotation {};
 };
 
 #endif //ASTRONAUT_H
--- src/Astronaut.cpp
+++ src/Astronaut.cpp
@@ -4,11 +4,60 @@
 
 #include "../include/Astronaut.h"
 
+#include <iostream>
+
 void Astronaut::update(float delta)
 {
-  DrawText("Astronaut testt", 20, 20, 20, GREEN);
-  structures::Vector2f dir = structures::Vector2f(IsKeyDown(KEY_D) - IsKeyDown(KEY_A), IsKeyDown(KEY_S) - IsKeyDown(KEY_W));
-  entityData->position = entityData->position + dir * 50.0f * delta;
+  std::cout << animationSpeed << std::endl;
+  DrawText("hello world", 10, 10, 10, WHITE);
+  currentTick += delta;
+  if (currentTick > 1.0f / animationSpeed)
+  {
+    animate();
+    currentTick = 0;
+  }
+  auto dir = structures::Vector2f(IsKeyDown(KEY_D) - IsKeyDown(KEY_A), IsKeyDown(KEY_S) - IsKeyDown(KEY_W)).normalized();
+  velocity = velocity + (dir - velocity) / 4.0f;
+  targetRotation = velocity.x * 10;
+  animationState = (dir.length() > 0.0f) ? RUNNING : IDLE;
+  entityData->rotation = targetRotation;
+  entityData->position = entityData->position + velocity * 75.0f * delta;
   entityData->dstRect.x = entityData->position.x;
   entityData->dstRect.y = entityData->position.y;
+  entityData->srcRect.width = (GetScreenToWorld2D(GetMousePosition(), *camera).x < entityData->position.x) ? 12 : -12;
+}
+
+void Astronaut::animate()
+{
+  switch (animationState)
+  {
+    case IDLE:
+      animationSpeed = 10.0f;
+      entityData->srcRect.x += 12;
+      if (entityData->srcRect.x > 36)
+      {
+        entityData->srcRect.x = 0;
+      }
+      break;
+    case RUNNING:
+      animationSpeed = 12.0f;
+      entityData->srcRect.x += 12;
+      if (entityData->srcRect.x > 132)
+      {
+        entityData->srcRect.x = 48;
+      }
+      else if (entityData->srcRect.x < 48)
+      {
+        entityData->srcRect.x = 48;
+      }
+      break;
+    case DEAD:
+      animationSpeed = 1;
+      break;
+  }
+}
+
+void Astronaut::setCamera(Camera2D * p_camera)
+{
+  camera = p_camera;
 }
