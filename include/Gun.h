e48c3311e061d9b287dbea85c1dfbaeec4e675d8
Alexey Pravilov <alexeypravilov@pub80-216.public.amu.edu.pl>
2025-02-02 16:57:40 +0100

    Added gun class

---
+++ include/Gun.h
@@ -0,0 +1,16 @@
+//
+// Created by Alexey Pravilov on 2.02.25.
+//
+
+#ifndef GUN_H
+#define GUN_H
+
+
+
+class Gun {
+
+};
+
+
+
+#endif //GUN_H
---
+++ src/Gun.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Alexey Pravilov on 2.02.25.
+//
+
+#include "../include/Gun.h"
