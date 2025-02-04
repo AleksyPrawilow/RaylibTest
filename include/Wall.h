26bb351eec1b5f20685da9515167a92ad556a628
Alexey Pravilov <alexeypravilov@pub80-216.public.amu.edu.pl>
2025-02-04 01:33:22 +0100

    Added walls

---
+++ include/Wall.h
@@ -0,0 +1,16 @@
+//
+// Created by Alexey Pravilov on 4.02.25.
+//
+
+#ifndef WALL_H
+#define WALL_H
+
+
+
+class Wall {
+
+};
+
+
+
+#endif //WALL_H
---
+++ src/Wall.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Alexey Pravilov on 4.02.25.
+//
+
+#include "../include/Wall.h"
