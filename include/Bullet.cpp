05e53de87f75aadca42cab619a420d606f13d726
Alexey Pravilov <alexeypravilov@pub92-106.public.amu.edu.pl>
2025-02-06 18:42:05 +0100

    Added bullet class

--- include/Bullet.cpp
+++ include/Bullet.cpp
@@ -3,3 +3,35 @@
 //
 
 #include "Bullet.h"
+
+Bullet::Bullet(EntityManager * p_manager, structures::EntityData * p_data) : Entity(p_manager, p_data)
+{
+  camera = entityManager->getDynamicCamera();
+}
+
+void Bullet::setSpeed(float p_speed)
+{
+  speed = p_speed;
+}
+
+void Bullet::setTargetRotation(float p_targetRotation)
+{
+  targetRotation = p_targetRotation;
+}
+
+void Bullet::update(float delta)
+{
+  entityData->position += fromAngle(targetRotation) * speed * delta;
+  entityData->dstRect.x = entityData->position.x;
+  entityData->dstRect.y = entityData->position.y;
+}
+
+void Bullet::render()
+{
+  Vector2 screenPos = GetWorldToScreen2D(entityData->position, *camera->getCamera());
+  if (screenPos.x < -64 || screenPos.x > 1400 || screenPos.y < -64  || screenPos.y > 784)
+  {
+    return;
+  }
+  Entity::render();
+}
--- include/Bullet.h
+++ include/Bullet.h
@@ -4,11 +4,21 @@
 
 #ifndef BULLET_H
 #define BULLET_H
+#include "Entity.h"
 
-
-
-class Bullet {
-
+class Bullet : public Entity
+{
+protected:
+  float speed = 5.0f;
+  float targetRotation = 0.0f;
+  DynamicCamera * camera{};
+public:
+  Bullet() : Entity() {};
+  Bullet(EntityManager * p_manager, structures::EntityData * p_data);
+  void setSpeed(float p_speed);
+  void setTargetRotation(float p_targetRotation);
+  void update(float delta) override;
+  void render() override;
 };
 
 
